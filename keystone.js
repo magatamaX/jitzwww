e635d4fb73faa1231b0e6ebb8b9e50798eeef00b
Tomohiro Hizaki <tomohiro.hizaki@isobar.com>
2018-02-21 15:45:13 +0900

    テンプレHTML埋込2&fonts!!

--- keystone.js
+++ keystone.js
@@ -10,19 +10,20 @@ var keystone = require('keystone');
 // and documentation.
 
 keystone.init({
-	'name': 'jitz',
-	'brand': '実務家Link',
+  'name': 'jitz',
+  'brand': '実務家Link',
 
-	'sass': 'public',
-	'static': 'public',
-	'favicon': 'public/favicon.ico',
-	'views': 'templates/views',
-	'view engine': 'pug',
+  'sass': 'public',
+  'static': 'public',
+  'favicon': 'public/favicon.ico',
+  'views': 'templates/views',
+  'view engine': 'pug',
 
-	'auto update': true,
-	'session': true,
-	'auth': true,
-	'user model': 'User',
+  'auto update': true,
+  'session': true,
+  'auth': true,
+  'user model': 'User',
+  'port': 36791,
 
 });
 
@@ -33,10 +34,10 @@ keystone.import('models');
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // for each request) should be added to ./routes/middleware.js
 keystone.set('locals', {
-	_: require('lodash'),
-	env: keystone.get('env'),
-	utils: keystone.utils,
-	editable: keystone.content.editable,
+  _: require('lodash'),
+  env: keystone.get('env'),
+  utils: keystone.utils,
+  editable: keystone.content.editable,
 });
 
 // Load your project's Routes
@@ -45,10 +46,10 @@ keystone.set('routes', require('./routes'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-	posts: ['posts', 'post-categories'],
-	galleries: 'galleries',
-	enquiries: 'enquiries',
-	users: 'users',
+  posts: ['posts', 'post-categories'],
+  galleries: 'galleries',
+  enquiries: 'enquiries',
+  users: 'users',
 });
 
 // Start Keystone to connect to your database and initialise the web server
