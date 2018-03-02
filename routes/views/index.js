648e7b877501388aaa15a2efbe2ffcd14d235e5f
Tomohiro Hizaki <tomohiro.hizaki@isobar.com>
2018-03-02 19:13:40 +0900

    navigations #1

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,4 +1,5 @@
 var keystone = require('keystone');
+// var navigations = require('./navi');
 var async = require('async');
 
 exports = module.exports = function (req, res) {
@@ -16,6 +17,7 @@ exports = module.exports = function (req, res) {
     posts: [],
     categories: [],
     recommend: [],
+    navigations: [],
   };
 
   // Load the posts（トップのスライダー）
@@ -86,6 +88,80 @@ exports = module.exports = function (req, res) {
       });
 
   });
+  
+  /* ------------------------------
+    ヘッダナビゲーション
+  -- --------------------------- */
+  // Load all categories
+  view.on('init', function (next) {
+
+    keystone.list('PostCategory').model.find().sort('category_No').exec(function (err, results) {
+
+      if (err || !results.length) {
+        return next(err);
+      }
+
+      locals.data.categories = results;
+
+      next();
+    });
+  });
+  
+  view.on('init', function(next) {
+    
+    // console.log(locals.data.categories.length);
+    
+    var cat_ary = [];
+    var cat = locals.data.categories;
+    var ldcl = locals.data.categories.length;
+    var query;
+    
+    for ( var i=0; i<ldcl; i++ ){
+      
+      cat_ary.push(cat[i]._id);
+      
+    }
+    console.log(cat_ary);
+    
+    // var query = keystone.list('Post').model.find().where('categories').populate('categories').sort('-publishedDate').limit(20);
+    var query = keystone.list('Post').model.where('category_No').populate('categories').find({"category_No":"5"}).sort('-publishedDate').limit(5);
+    // var t = keystone.list('Post').model.find({
+    //   categories : {
+    //     category_No : 2,
+    //   },
+    // }).limit(6)
+    
+    // var t = keystone.list('Post').paginate({
+    //   page: 1,
+    //   perPage: 6,
+    //   maxPages: 1,
+    //   filters: {
+    //     state: 'published',
+    //     categories: locals.data.category,
+    //   },
+    // })
+    //   .sort('-publishedDate')
+    //   .populate('categories');
+    // 
+    // if (locals.data.category) {
+    //   t.where('categories').in([locals.data.category]);
+    // }
+
+    query.exec(function (err, results) {
+      locals.data.navigations = results;
+      for (var j=0; j<5; j++){
+        console.log(results[j].categories[0].category_No);
+      }
+      next(err);
+    });
+    // console.log(locals.data.categories);
+    
+    
+    // next();
+    
+  });
+  
+  
 
   // Render the view
   view.render('index');
---
+++ routes/views/navi.js
@@ -0,0 +1,19 @@
+var keystone = require('keystone');
+var async = require('async');
+var _ = require('lodash');
+
+exports = module.exports = function(next){
+  
+  // var view = new keystone.View(req, res);
+  // var locals = res.locals;
+  // locals.data.navigations = [];
+
+
+
+  var wawawa = 'wawawawa!!';
+  console.log(wawawa);
+  
+
+  next();
+  
+}
